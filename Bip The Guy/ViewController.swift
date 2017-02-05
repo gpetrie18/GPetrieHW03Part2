29d807103d696b6b476ff312b895b6d83e0d564e
CSOM <csom@bc-macbookpro-38194d.local>
2017-02-05 18:32:20 -0500

    4.1 Complete

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageToPunch: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +23,16 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func libraryPressed(_ sender: UIButton) {
+
+    }
+    
+    @IBAction func cameraPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        print("Hey! You Just Pressed the Image")
+    }
+    
 }
 
